6dd5f835adcbf309416b382c760f0eed25d9beb0
SahasOP <sahasprajapati54@gmail.com>
2025-12-10 22:45:19 +0530

    changes

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "https://skillhub-backend-mauve.vercel.app/api/v1"
-
-const axiosInstance = axios.create()
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+});
 
 export default axiosInstance;
